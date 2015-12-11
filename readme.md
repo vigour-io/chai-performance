6426d9fd2e7703d707ac18d7c294b79f69bd786b
Jim de Beer <jim@vigour.io>
2015-12-11 16:52:43 +0100

    fixed eprf plugin

--- readme.md
+++ readme.md
@@ -10,19 +10,19 @@ var chai = require('chai')
 chai.use(require('chai-performance'))
 
 describe('chai-performance', function () {
-  it('division should be as fast a multiplication', function (done) {
+  it('division should be as fast as multiplication', function (done) {
     this.timeout(50e3)
     var amount = 1e6
     expect(function () {
-      for (var i = 0; i < amount; i++) {
-        i = i * 10
+      for (var i = 0, j; i < amount; i++) {
+        j = i * 10
       }
     }).performance({
       margin: 1,
       loop:10,
       method: function () {
-        for (var i = 0; i < amount; i++) {
-          i = i / 10
+        for (var i = 0, j; i < amount; i++) {
+          j = i / 10
         }
       }
     }, done)
--- test/common/index.js
+++ test/common/index.js
@@ -2,19 +2,19 @@
 chai.use(require('../../lib'))
 
 describe('chai-performance', function () {
-  it('division should be as fast a multiplication', function (done) {
+  it('division should be as fast as multiplication', function (done) {
     this.timeout(50e3)
     var amount = 1e4
     expect(function () {
-      for (var i = 0; i < amount; i++) {
-        i = i * 10
+      for (var i = 0, j; i < amount; i++) { //eslint-disable-line
+        j = i * 10
       }
     }).performance({
       margin: 1,
-      loop: 2,
+      loop: 20,
       method () {
-        for (var i = 0; i < amount; i++) {
-          i = i / 10
+        for (var i = 0, j; i < amount; i++) { //eslint-disable-line
+          j = i / 10
         }
       }
     }, done)
