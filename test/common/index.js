cdd6f1d498ee8234df75e77405506d44dc8bf9b1
Jim de Beer <jim@vigour.io>
2015-12-11 16:39:22 +0100

    removed bluebird and promises

--- test/common/index.js
+++ test/common/index.js
@@ -4,14 +4,14 @@ chai.use(require('../../lib'))
 describe('chai-performance', function () {
   it('division should be as fast a multiplication', function (done) {
     this.timeout(50e3)
-    var amount = 1e6
+    var amount = 1e4
     expect(function () {
       for (var i = 0; i < amount; i++) {
         i = i * 10
       }
     }).performance({
       margin: 1,
-      loop: 10,
+      loop: 2,
       method () {
         for (var i = 0; i < amount; i++) {
           i = i / 10
