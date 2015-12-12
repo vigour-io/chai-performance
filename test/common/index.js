d63b65fc60938bd75ad613bb7c5bf81e01fd9f51
Jim de Beer <jim@vigour.io>
2015-12-13 00:14:18 +0100

    implemented feedback system

--- test/common/index.js
+++ test/common/index.js
@@ -1,17 +1,19 @@
 'use strict'
-chai.use(require('../../lib'))
-
+var lib = require('../../lib')
+lib.log = true
+chai.use(lib)
+// base erin :) so nice
 describe('chai-performance', function () {
   it('division should be as fast as multiplication', function (done) {
     this.timeout(50e3)
-    var amount = 1e4
+    var amount = 1e3
     expect(function () {
       for (var i = 0, j; i < amount; i++) { //eslint-disable-line
         j = i * 10
       }
     }).performance({
-      margin: 2,
-      loop: 20,
+      margin: 1,
+      loop: 100,
       method () {
         for (var i = 0, j; i < amount; i++) { //eslint-disable-line
           j = i / 10
